be81540786df147c3cb150bdc11a4035cfa714e7
Justin Riggio <nicemindz@gmail.com>
2022-04-21 20:02:08 -0400

    clean up

--- renderer.js
+++ renderer.js
@@ -1,14 +1,3 @@
-// console.log(database);
-
-// let db = new database('PanelKiosk');
-// let database = new Dexie('PanelKiosk');
-// database.version(1).stores({    
-//     tracking: '++id, page, timestamp, status, notes'
-// });
-// database.tracking.add({page : window.location.href, timestamp : Date.now(), status : 'loaded', notes : 'Panel Kiosk'});
-
-
-
 // Called when message received from main process
 // window.api.receive("fromMain", (data) => {
 //     console.log(`Received ${data} from main process`);
